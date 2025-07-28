eed7604bb37dbfba8dcb7b4bc291ce7e6a99c61a
KavinArasan-FTSM <a195661@siswa.ukm.edu.my>
2025-07-28 19:24:00 +0800

    Update question1.py

--- src/question1.py
+++ src/question1.py
@@ -1,1 +1,13 @@
+# Problem 1 - Sorting
 
+def custom_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+# Test
+print(custom_sort([21, 400, 8, -3, 77, 99, -16, 55, 111, -36, 28]))
+
+# Time complexity is O(n^2) while Space is O(1).
