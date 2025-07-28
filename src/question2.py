cb5ab88233c1c6400564a0fde4b5f2abbf398c30
KavinArasan-FTSM <a195661@siswa.ukm.edu.my>
2025-07-28 19:30:56 +0800

    Update question2.py

--- src/question2.py
+++ src/question2.py
@@ -1,1 +1,10 @@
-
+# Problem 2 - FizzBuzz
+for i in range(1, 101):  # Numbers from 1 to 100
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=", ")
+    elif i % 3 == 0:
+        print("Fizz", end=", ")
+    elif i % 5 == 0:
+        print("Buzz", end=", ")
+    else:
+        print(i, end=", ")
